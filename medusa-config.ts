06db0fb58a3b73b7d84b02e618244baf9623cc96
MarcMateo100 <marcmateo97@gmail.com>
2026-01-01 17:06:59 +0100

    Fix:version yarn2

--- medusa-config.ts
+++ medusa-config.ts
@@ -10,8 +10,8 @@ export default defineConfig({
     databaseUrl: process.env.DATABASE_URL,
     http: {
       storeCors: "*",
-      adminCors: process.env.ADMIN_CORS,
-      authCors: process.env.AUTH_CORS,
+      adminCors: "*",
+      authCors: "*",
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
