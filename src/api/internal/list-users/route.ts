e2d7bd39b716262f563dfb9102c9684f31f11b0f
MarcMateo100 <marcmateo97@gmail.com>
2026-01-06 23:36:10 +0100

    Fix:delete route-ts

--- src/api/internal/list-users/route.ts
+++
@@ -1,9 +0,0 @@
-import { MedusaRequest, MedusaResponse } from "@medusajs/medusa";
-
-export async function GET(req: MedusaRequest, res: MedusaResponse) {
-  const userService = req.scope.resolve("userService");
-
-  const users = await userService.list();
-
-  return res.json({ users });
-}
