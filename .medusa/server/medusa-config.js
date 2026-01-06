2bfe9131dbb95258a3aeef47bf9f1a38235f9266
MarcMateo100 <marcmateo97@gmail.com>
2026-01-07 00:24:00 +0100

    Fix:medusa-config

--- .medusa/server/medusa-config.js
+++ .medusa/server/medusa-config.js
@@ -7,6 +7,14 @@ exports.default = (0, utils_1.defineConfig)({
     projectConfig: {
         databaseUrl: process.env.DATABASE_URL,
         http: {
+          port: process.env.PORT || 9000,
+          storeCors: "*",
+          adminCors: "*",
+          authCors: "*",
+          jwtSecret: process.env.JWT_SECRET || "supersecret",
+          cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+        }
+
             storeCors: "*",
             adminCors: "*",
             authCors: "*",
@@ -15,7 +23,7 @@ exports.default = (0, utils_1.defineConfig)({
         },
     },
     admin: {
-        disable: true,
+        disable: false,
     },
 });
 //# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWVkdXNhLWNvbmZpZy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL21lZHVzYS1jb25maWcudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSxxREFBaUU7QUFFakUsT0FBTyxDQUFDLEdBQUcsQ0FBQyxrQ0FBa0MsQ0FBQyxDQUFDO0FBR2hELElBQUEsZUFBTyxFQUFDLE9BQU8sQ0FBQyxHQUFHLENBQUMsUUFBUSxJQUFJLGFBQWEsRUFBRSxPQUFPLENBQUMsR0FBRyxFQUFFLENBQUMsQ0FBQTtBQUU3RCxrQkFBZSxJQUFBLG9CQUFZLEVBQUM7SUFDMUIsYUFBYSxFQUFFO1FBQ2IsV0FBVyxFQUFFLE9BQU8sQ0FBQyxHQUFHLENBQUMsWUFBWTtRQUNyQyxJQUFJLEVBQUU7WUFDSixTQUFTLEVBQUUsR0FBRztZQUNkLFNBQVMsRUFBRSxHQUFHO1lBQ2QsUUFBUSxFQUFFLEdBQUc7WUFDYixTQUFTLEVBQUUsT0FBTyxDQUFDLEdBQUcsQ0FBQyxVQUFVLElBQUksYUFBYTtZQUNsRCxZQUFZLEVBQUUsT0FBTyxDQUFDLEdBQUcsQ0FBQyxhQUFhLElBQUksYUFBYTtTQUN6RDtLQUVGO0lBRUQsS0FBSyxFQUFFO1FBQ0wsT0FBTyxFQUFFLElBQUk7S0FDZDtDQUNGLENBQUMsQ0FBQSJ9
