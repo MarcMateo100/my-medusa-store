7a3539a4c3905ee283ac729674110a21b968f0c1
MarcMateo100 <marcmateo97@gmail.com>
2026-01-07 22:19:20 +0100

    Fix:medusa-config2

--- medusa-config.ts
+++ medusa-config.ts
@@ -7,7 +7,6 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 export default defineConfig({
   projectConfig: {
-    databaseUrl: process.env.DATABASE_URL,
     http: {
       storeCors: "*",
       adminCors: "*",
